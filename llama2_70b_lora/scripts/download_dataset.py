55d849662dabc3215a35612a98e8d1148b50b85b
Daniel Altunay <daniel.altunay@flex.ai>
2024-10-03 08:01:11 +0000

    add base scripts

---
+++ llama2_70b_lora/scripts/download_dataset.py
@@ -0,0 +1,19 @@
+import argparse
+from huggingface_hub import snapshot_download
+
+parser = argparse.ArgumentParser(description="Download dataset using Hugging Face Hub")
+parser.add_argument(
+    "--data_dir",
+    type=str,
+    required=True,
+    help="Local directory to download the dataset to",
+)
+
+args = parser.parse_args()
+
+snapshot_download(
+    "regisss/scrolls_gov_report_preprocessed_mlperf_2",
+    local_dir=args.data_dir,
+    local_dir_use_symlinks=False,
+    repo_type="dataset",
+)
---
+++ llama2_70b_lora/scripts/download_model.py
@@ -0,0 +1,18 @@
+import argparse
+from huggingface_hub import snapshot_download
+
+parser = argparse.ArgumentParser(description="Download model using Hugging Face Hub")
+parser.add_argument(
+    "--model_dir",
+    type=str,
+    required=True,
+    help="Local directory to download the model to",
+)
+
+args = parser.parse_args()
+
+snapshot_download(
+    "regisss/llama2-70b-fused-qkv-mlperf",
+    local_dir=args.model_dir,
+    local_dir_use_symlinks=False,
+)
